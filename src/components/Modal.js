55e31d708c9f51ac1a58399e86bec9b67cc8cfea
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-02-06 22:47:57 +0530

    modal

---
+++ src/components/Modal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Modal = () => {
+    return (
+        <div className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover bg-dark backdrop-filter backdrop-blur-md bg-opacity-20" >
+            <div className="absolute bg-black opacity-80 inset-0 z-0"></div>
+            <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">
+
+                <div className="">
+                    <div className="text-center p-5 flex-auto justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 -m-1 flex items-center text-green-500 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-16 h-16 flex items-center text-green-500 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                        </svg>
+                        <h2 className="text-xl py-4 mt-4 text-black">Accept the voucher to continue</h2>
+                    </div>
+                    <div className="p-3  mt-2 text-center space-x-4 md:block">
+                        <button className="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
+                            Cancel
+                        </button>
+                        <button className="mb-2 md:mb-0 bg-green-500 border border-green-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Accept</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Modal;
