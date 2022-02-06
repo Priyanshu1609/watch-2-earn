335b7212d69b71858c2c467de09f7723bd45375d
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-02-06 22:48:24 +0530

    modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const Modal = () => {
+    const router = useRouter();
+
     return (
         <div className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover bg-dark backdrop-filter backdrop-blur-md bg-opacity-20" >
             <div className="absolute bg-black opacity-80 inset-0 z-0"></div>
@@ -17,10 +20,10 @@ const Modal = () => {
                         <h2 className="text-xl py-4 mt-4 text-black">Accept the voucher to continue</h2>
                     </div>
                     <div className="p-3  mt-2 text-center space-x-4 md:block">
-                        <button className="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
+                        <button onClick={() => router.push('/')} className="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
                             Cancel
                         </button>
-                        <button className="mb-2 md:mb-0 bg-green-500 border border-green-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600">Accept</button>
+                        <button className="mb-2 md:mb-0 bg-green-500 border border-green-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:opacity-80">Accept</button>
                     </div>
                 </div>
             </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -66,7 +66,7 @@ function Index() {
                 <p className="">Nike Air</p>
                 <p className="">Jordan</p>
               </div>
-              <div className="p-1 ml-8 bg-green-500 text-gray-100 rounded-md shadow-lg w-20 h-5 flex" onClick={()=> router.push('/product') }>
+              <div className="p-1 ml-8 bg-green-500 text-gray-100 rounded-md shadow-lg w-20 h-5 flex hover:cursor-pointer" onClick={()=> router.push('/product') }>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-2 w-2 my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
